9226a39cf4a8a5b414d9f1aa9ee47b82774db2c0
sajidkhattak1996 <sajidktkkpk@gmail.com>
2019-09-14 17:08:00 +0500

    sajid

--- project/admin.php
+++ project/admin.php
@@ -1,3 +1,31 @@
+<style type="text/css">
+	h1{
+		text-align: center;
+		color: deeppink;
+		font-size: 50px;
+	}
+	h5{
+		color: blue;
+
+	}
+	h6{
+		color: red;
+		font-size: 40px;
+		text-align: center;
+	}
+	h4{
+		word-spacing: 5px;
+		color: red;
+		font-size: 40px;
+		font-style: italic;
+		font-family: artial;
+	}	
+
+</style>
+
+
+
+
 <?php  
 
 $name  =$_POST['name'];
@@ -6,27 +34,51 @@ $password = $_POST['password'];
 
 echo "<h5>".$name."<br>".$email."<br>".$password."<br><br>";
 
-
+//parameter user  for the database connection 
 $server ="localhost";
 $user ="root";
 $pass ="";
 $db ="hostel";
 
+//database connection are created 
 $con =mysqli_connect($server,$user,$pass,$db);
-echo "connection are succeffully created ";
 
-$query1 ="select * from admin ";
+//this query are use for the searching for the database of the admin and his password 
+//use for the security purpose 
+$query1 ="SELECT * FROM admin WHERE name='$name' and email='$email' and password='$password'";
 $result =$con->query($query1);
 
-echo "<br>query are executed <br>";
+//condition for the password match with the datbase 
+if ($result->num_rows > 0) 
+{
+	while ($row = $result->fetch_assoc()) 
+	{
+		if ($row["name"]==$name  &&  $row["email"]==$email  && $row["password"]==$password ) 
+		{
+			//here start and creating the session variable 
+			session_start();
+			$_SESSION['n']=$name;
+			echo "n===".$_SESSION['n'];
 
-
-while ($row =$result->fetch_assoc()) {
-	
-	echo "<h2>Name:    ".$row['name']."&nbsp&nbsp&nbsp"."Email : ".$row['email']."&nbsp&nbsp&nbsp".$row['password'];
-
+			echo "<h2>The Admin Page is Processing for You ... </h2>";
+			
+		}
+		else
+		{
+			echo "<h4>No Admin Record are found in the Database <br>Please Enter again the correct Detail of Admin !</h4>";			
+		}
+	}
 
 
 }
+else
+{
+	echo "<h4>No Admin Record are found in the Database <br>Please Enter again the correct Detail of Admin !</h4>";
+}
+
+
+$result->close();
+$con->close();
+
 
 ?>
